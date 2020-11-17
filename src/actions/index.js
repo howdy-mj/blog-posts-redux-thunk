f2a96cf9434f375e719bdcbd22a5f69108e7087b
kmj <hi.minjungkim@gmail.com>
2020-11-17 22:30:48 +0900

    Memoizing functions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
+import _ from 'lodash';
 
 // it's a function that return function
 export const fetchPosts = () => async (dispatch) => {
@@ -7,7 +8,17 @@ export const fetchPosts = () => async (dispatch) => {
   dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
 
-export const fetchUser = (id) => async (dispatch) => {
+export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch);
+const _fetchUser = _.memoize(async (id, dispatch) => {
+  // 같은 id를 한 번만 호출
   const response = await jsonPlaceholder.get(`/users/${id}`);
   dispatch({ type: 'FETCH_USER', payload: response.data });
-};
+});
+
+// 밖의 함수에 memoize를 하든, 안에 하든 결국 동적으로 생성되는 것이기 때문에 수십번 호출
+// export const fetchUser = function (id) {
+//   return _.memoize(async function (dispatch) {
+//     const response = await jsonPlaceholder.get(`/users/${id}`);
+//     dispatch({ type: 'FETCH_USER', payload: response.data });
+//   });
+// };
